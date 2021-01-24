0c826f1d8232b9c446f7b43405cb95c4193b2dcf
a_frear <amy.frear@gmail.com>
2021-01-24 12:06:30 -0500

    Added tests

---
+++ src/LikeButton/LikeButton.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LikeButton from './LikeButton';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<LikeButton />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
