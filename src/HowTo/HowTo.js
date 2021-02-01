b7d7cb6680e85c2138375d31ba626e057cdffcf8
a_frear <amy.frear@gmail.com>
2021-02-01 10:31:08 -0500

    Updated copy for HowTo

--- src/HowTo/HowTo.js
+++ src/HowTo/HowTo.js
@@ -3,9 +3,9 @@ import React from 'react'
 const HowTo = () => {
   return (
     <div class="how-to">
-            <h2 className='how-to-desc'>Find the Eyes to Watch Videos for your Eyes</h2>
+            <h2 className='how-to-desc'> Watch Videos for your Eyes</h2>
             <h2 className='how-to-desc'>Submit your own Eye Content</h2>
-            <h2 className='how-to-desc'>Log in to Connect with other Eye Lovers</h2>
+            <h2 className='how-to-desc'>Connect with other Eye Lovers</h2>
     </div>
   )
 }
