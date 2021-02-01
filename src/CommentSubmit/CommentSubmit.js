98a5376fbbfcd219b8aa08852018c1e0afa3dab2
a_frear <amy.frear@gmail.com>
2021-02-01 08:33:39 -0500

    Update CommentSubmit.js

--- src/CommentSubmit/CommentSubmit.js
+++ src/CommentSubmit/CommentSubmit.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { useAuth0 } from "@auth0/auth0-react"
 
 const CommentSubmit = (props) => {
-    const { isAuthenticated } = useAuth0()
+    const { user, isAuthenticated } = useAuth0()
           return (
             isAuthenticated && (
             <div>
                 <form className='comment'  onSubmit={e =>props.handleSubmit(e)}> 
-                        <label>Comment:</label>
+                        <label>Comment as ${user.nickname}:</label>
                         <textarea type='text' id='new-comment' name='new-comment' required></textarea>
                         <div>
                         <button className='comment-submit'>Submit</button> 
