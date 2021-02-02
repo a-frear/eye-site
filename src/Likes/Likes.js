9f8252b04218be03dc61e712a26a00fd78f9773b
a_frear <amy.frear@gmail.com>
2021-02-01 21:20:45 -0500

    Likes css

--- src/Likes/Likes.js
+++ src/Likes/Likes.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./LikeButton.css";
+import "./Likes.css";
 import { useAuth0 } from "@auth0/auth0-react";
 import LikeButton from '../LikeButton/LikeButton';
 import config from "../config";
