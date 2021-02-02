824f7aa58aa3bd0a5b3b91ec58775256154bc762
a_frear <amy.frear@gmail.com>
2021-02-01 21:51:36 -0500

    like button state

--- src/LikeButton/LikeButton.js
+++ src/LikeButton/LikeButton.js
@@ -2,10 +2,13 @@ import React, { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import likeButton from "../img/white-like-button.png";
 import pinkLikeButton from "../img/pink-like-button.png";
+import config from "../config";
 
 const LikeButton = (props) => {
   const [likes ] = useState(props.likes);
-  const { user, isAuthenticated } = useAuth0();
+  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+
+  console.log("like button state likes" + likes)
 
   const findUserLikes = likes.filter(
     (like) => like.user_name === user.nickname
@@ -13,6 +16,35 @@ const LikeButton = (props) => {
   const findUserLikesThisVideo = findUserLikes.filter(
     (like) => like.video_id === props.vidId
   )
+
+const handleClick = async (e) => {
+  e.preventDefault();
+  if (findUserLikesThisVideo.length === 0) {
+    const newLike = {
+      video_id: props.vidId,
+      user_name: user.nickname,
+    };
+    const token = await getAccessTokenSilently();
+    fetch(config.API_ENDPOINT_likes, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(newLike),
+    })
+      .then((res) => {
+        if (!res.ok) return res.json().then((e) => Promise.reject(e));
+        return res.json();
+      })
+      .then((like) => {
+        props.addLike(like);
+      })
+      .catch((error) => {
+        console.error({ error });
+      });
+  }
+};
 
   const buttonSrc = findUserLikesThisVideo.length === 0 ? likeButton : pinkLikeButton;
   return (
@@ -22,7 +54,7 @@ const LikeButton = (props) => {
               src={buttonSrc}
               className="like-button"
               id="likeVideo"
-              onClick={(e) => props.handleClick(e)}
+              onClick={(e) => handleClick(e)}
             />
     )
   );
--- src/Likes/Likes.js
+++ src/Likes/Likes.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import "./Likes.css";
-import { useAuth0 } from "@auth0/auth0-react";
 import LikeButton from '../LikeButton/LikeButton';
 import config from "../config";
 
@@ -26,45 +25,8 @@ const Likes = (props) => {
   }, []);
 
   function addLike(like) {
-      setLikes([...likes, like]);
-  }
-
-  const handleClick = async (e) => {
-    e.preventDefault();
-    const { user, getAccessTokenSilently } = useAuth0();
-    const findUserLikes = likes.filter(
-      (like) => like.user_name === user.nickname
-    );
-    const findUserLikesThisVideo = findUserLikes.filter(
-      (like) => like.video_id === props.vidId
-    )
-    if (findUserLikesThisVideo.length === 0) {
-      e.preventDefault();
-      const token = await getAccessTokenSilently();
-      const newLike = {
-        video_id: props.vidId,
-        user_name: user.nickname,
-      };
-      fetch(config.API_ENDPOINT_likes, {
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify(newLike),
-      })
-        .then((res) => {
-          if (!res.ok) return res.json().then((e) => Promise.reject(e));
-          return res.json();
-        })
-        .then((like) => {
-          addLike(like);
-        })
-        .catch((error) => {
-          console.error({ error });
-        });
-    }
-  };
+    setLikes([...likes, like]);
+}
 
   const getLikesForVideo = (likes = [], video_id) =>
     likes.filter((like) => like.video_id === video_id);
@@ -76,7 +38,7 @@ const Likes = (props) => {
         <form className="like">
           <label>{likesForVideo.length} winks</label>
           <div>
-          <LikeButton vidId={props.vidId} likes={likes} handleClick={handleClick}/>
+          <LikeButton vidId={props.vidId} likes={likes} addLike={addLike} />
           </div>
           {/* <div className="imageBox">
             <div className="imageInn">
